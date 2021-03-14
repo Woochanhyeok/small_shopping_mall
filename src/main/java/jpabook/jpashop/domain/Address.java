debdd155ca4452e5344b884c982ed4fed07bf5c5
Woo <dncksgur1@naver.com>
2021-03-15 00:28:24 +0900

    order

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
@@ -12,4 +13,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    public Address() {
+        super();
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -17,7 +17,7 @@ public class Delivery {
     private Order order;
 
     @Embedded
-    private Address adress;
+    private Address address;
 
     @Enumerated(EnumType.STRING)
     private DeliveryStatus status;
